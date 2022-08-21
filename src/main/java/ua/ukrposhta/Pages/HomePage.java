54e86594b4478c78affe9029502d39272d6677ae
Vladimir Kuznetsov <vladimir.kuznetsov@white-test.com>
2022-08-21 22:14:58 +0300

    Two language tests completed

--- src/main/java/ua/ukrposhta/Pages/HomePage.java
+++ src/main/java/ua/ukrposhta/Pages/HomePage.java
@@ -2,64 +2,115 @@ package ua.ukrposhta.Pages;
 
 import com.codeborne.selenide.SelenideElement;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static com.codeborne.selenide.Selenide.$x;
 
 public class HomePage {
 
 
 
-    private final SelenideElement contactCenterRu = $x("/html/body/header/div[1]/div/div/div/p[2]/a");
-    //private final String EN_URL = WebDriverRunner.getWebDriver().getCurrentUrl();
+    private final SelenideElement contactCenter = $x("/html/body/header/div[1]/div/div/div/p[2]/a");
     private final SelenideElement languageDropdownButton = $x("/html/body/header/div[2]/div[1]/div/div[4]/span");
     private final SelenideElement firstLanguageButton = $x("/html/body/header/div[2]/div[1]/div/div[4]/div/div/a[1]");
     private final SelenideElement secondLanguageButton = $x("/html/body/header/div[2]/div[1]/div/div[4]/div/div/a[2]");
-    private final SelenideElement firstSlickButton = $x("/html/body/div[3]/div/div[1]/ul/li[1]/button");
-    private final SelenideElement secondSlickButton = $x("/html/body/div[3]/div/div[1]/ul/li[2]/button");
-    private final SelenideElement thirdSlickButton = $x("/html/body/div[3]/div/div[1]/ul/li[3]/button");
+    private final SelenideElement firstSlickButton = $x("/html/body/div[3]/div/div[1]/ul/li[1]");
+    private final SelenideElement secondSlickButton = $x("/html/body/div[3]/div/div[1]/ul/li[2]");
+    private final SelenideElement thirdSlickButton = $x("//html/body/div[3]/div/div[1]/ul/li[3]");
     private final SelenideElement sliderItemContent = $x("/html/body/div[3]/div/div[1]/div/div/a[1]/div[2]/div");
-    List<String> initialValues = new ArrayList<String>();
-    List<String> actualValues= new ArrayList<String>();
 
 
+    /**
+     * Click on the language selection button
+     */
     public void openLanguageSelectionDropdown() {
         languageDropdownButton.click();
-    }
-
-    public void selectRuLanguage() {
-        firstLanguageButton.click();
     }
 
     /**
      * Comparing actual and expected values provided in the language selection dropdown
      */
     public boolean languageDropdownValuesCompare(String valueOne, String valueTwo){
-        String firstValue = firstLanguageButton.getText(); // Get text of the first dropdown value
-        String secondValue = secondLanguageButton.getText(); // Get text of the second dropdown value
+        String firstValue = firstLanguageButton.getText();
+        String secondValue = secondLanguageButton.getText();
 
-        initialValues.add(valueOne); // Adding first reference dropdown value to an array
-        initialValues.add(valueTwo); // Adding second reference dropdown value to an array
-
-        actualValues.add(firstValue); // Adding  first actual dropdown value to an array
-        actualValues.add(secondValue); // Adding  second actual dropdown value to an array
-
-        for(int i = 0; i < actualValues.size(); i++){                   // Arrays comparison
-            if(actualValues.get(i).equals(initialValues.get(i))){
-                return true;
-            }
+        if(firstValue.equals(valueOne) && secondValue.equals(valueTwo)){
+            return true;
         } return false;
-
     }
 
     /**
-     * Comparing actual and expected URL
+     * Click on the "RU" button of the dropdown-list
+     */
+    public void selectRuLanguage() {
+        firstLanguageButton.click();
+    }
+
+    /**
+     * Click on the "EN" button of the dropdown-list
+     */
+    public void selectEnLanguage() {
+        secondLanguageButton.click();
+    }
+
+    /**
+     * Check if the website language changed to RU
      */
     public boolean ruLanguageAppliedCheck (){
-        String ruText = contactCenterRu.getText();
+        String ruText = contactCenter.getText();
         return ruText.equals("контакт-центр");
     }
 
+    /**
+     * Check if the website language changed to RU
+     *
+     */
+    public boolean enLanguageAppliedCheck () {
+        String enText = contactCenter.getText();
+        return enText.equals("contact center");
+    }
+
+    /**
+     * Click on the first slick button
+     */
+    public void clickOnTheFirstSlickButton() {
+        firstSlickButton.click();
+    }
+
+    /**
+     * Click on the second slick button
+     */
+    public void clickOnTheSecondSlickButton() {
+        secondSlickButton.click();
+    }
+
+    /**
+     * Click on the third slick button
+     */
+    public void clickOnTheThirdSlickButton() {
+        thirdSlickButton.click();
+    }
+
+    /**
+     * Verify first slider item content header
+     */
+    public boolean firstSliderContentHeader() {
+        String headerText = sliderItemContent.getText();
+        return headerText.equals("Замовляйте онлайн");
+    }
+
+    /**
+     * Verify second slider item content header
+     */
+    public boolean secondSliderContentHeader() {
+        String headerText = sliderItemContent.getText();
+        return headerText.equals("Купуйте з користю для себе та країни");
+    }
+
+    /**
+     * Verify third slider item content header
+     */
+    public boolean thirdSliderContentHeader() {
+        String headerText = sliderItemContent.getText();
+        return headerText.equals("Продавайте товари за кордон");
+    }
 }
 
--- src/test/java/HomePageTests.java
+++ src/test/java/HomePageTests.java
@@ -1,5 +1,6 @@
 import Utils.WebDriverFactory;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ua.ukrposhta.Pages.HomePage;
 
@@ -7,23 +8,44 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class HomePageTests {
 
-    @Test
-    public void changeLanguage () throws InterruptedException {
+    @BeforeMethod
+    public void setUp() {
         WebDriverFactory driver = new WebDriverFactory();
         driver.GetDriver("");
         open("https://ukrposhta.ua/");
+    }
+
+    @Test
+    public void changeLanguageToRu () throws InterruptedException {
         HomePage homePage = new HomePage();
         Thread.sleep(3000);
         homePage.openLanguageSelectionDropdown();
         Thread.sleep(3000);
-        //Assert.assertTrue(homePage.languageDropdownValuesCompare("RU", "EN"));
+        Assert.assertTrue(homePage.languageDropdownValuesCompare("RU", "EN"));
         homePage.selectRuLanguage();
         Thread.sleep(3000);
-
         Assert.assertTrue(homePage.ruLanguageAppliedCheck());
         Thread.sleep(3000);
-
-
-
+        homePage.openLanguageSelectionDropdown();
+        Thread.sleep(3000);
+        Assert.assertTrue(homePage.languageDropdownValuesCompare("UA", "EN"));
     }
+
+    @Test
+    public void changeLanguageToEn () throws InterruptedException {
+        HomePage homePage = new HomePage();
+        Thread.sleep(3000);
+        homePage.openLanguageSelectionDropdown();
+        Thread.sleep(3000);
+        Assert.assertTrue(homePage.languageDropdownValuesCompare("RU", "EN"));
+        homePage.selectEnLanguage();
+        Thread.sleep(3000);
+        Assert.assertTrue(homePage.enLanguageAppliedCheck());
+        Thread.sleep(3000);
+        homePage.openLanguageSelectionDropdown();
+        Thread.sleep(3000);
+        Assert.assertTrue(homePage.languageDropdownValuesCompare("UA", "RU"));
+    }
+
+    
 }
